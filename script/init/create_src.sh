8d9849f0e0aaf5f10c685b086f541104599c7024
egoma <webeng.perilla@gmail.com>
2024-03-05 12:24:48 +0900

    rails newオプションの共通化 #5

--- script/init/create_src.sh
+++ script/init/create_src.sh
@@ -7,7 +7,7 @@ source .env
 mkdir src
 
 docker compose run --rm app bash -c "
-rails _${RAILS_VERSION}_ new . --database mysql --skip-bundle --skip
+rails _${RAILS_VERSION}_ new . --skip-bundle --skip ${RAILS_NEW_OPTION}
 "
 
 sudo chown -R $(id -u):$(id -g) src
